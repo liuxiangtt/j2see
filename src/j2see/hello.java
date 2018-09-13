750c9443c325a722847f95669bea252b72064cac
liuxiang <8926472@qq.com>
2018-09-13 16:23:09 +0800

    test2

--- src/j2see/hello.java
+++ src/j2see/hello.java
@@ -3,7 +3,8 @@ package j2see;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello!");
-
+		System.out.println("hello2!");
+		System.out.println("hello3!");
 	}
 
 }
